991f2bc373eeecf6b8653778e7d64f6209126c03
Kai Aihara <AiharaKai@lifull.com>
2025-09-16 15:51:12 +0900

    Enable to work with skaffold

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -4,13 +4,24 @@
 package v1
 
 import (
+	"fmt"
+	"os"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
+func apiGroup() string {
+	defaultGroup := "spinnaker.kaidotdev.github.io"
+	if v, ok := os.LookupEnv("VARIANT"); ok {
+		return fmt.Sprintf("%s.%s", v, defaultGroup)
+	}
+	return defaultGroup
+}
+
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "spinnaker.kaidotdev.github.io", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: apiGroup(), Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
